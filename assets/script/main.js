69978ff9aeb1d8bbbbb438e20629659d67eebdb2
zibanoori <noorizibaa@gmail.com>
2026-08-28 22:16:22 +0330

    Add sidebar toggle and icon switch

--- assets/script/main.js
+++ assets/script/main.js
@@ -2,3 +2,14 @@ const sidebar = document.getElementById('sidebar');
 const toggleBtn = document.getElementById('sidebar-toggle');
 const toggleIcon = toggleBtn.querySelector('i');
 
+toggleBtn.addEventListener('click', () => {
+    sidebar.classList.toggle('collapsed');
+  
+    if (sidebar.classList.contains('collapsed')) {
+        toggleIcon.classList.remove('fa-chevron-left');
+        toggleIcon.classList.add('fa-chevron-right');
+    } else {
+        toggleIcon.classList.remove('fa-chevron-right');
+        toggleIcon.classList.add('fa-chevron-left');
+    }
+});
